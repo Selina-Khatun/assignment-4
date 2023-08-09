c8f9cec0d08c52ff6695f2241cd6ae0f594ee8c3
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2023-08-09 23:54:51 +0600

    all problems solved

--- assignment.js
+++ assignment.js
@@ -7,7 +7,7 @@ function cubeNumber(number) {
       return result;
    }
 }
-console.log(cubeNumber(p));
+// console.log(cubeNumber(p));
 // console.log(cubeNumber(3));
 
 
@@ -36,3 +36,63 @@ function matchFinder(string1, string2) {
 // const string2 = "pet";
 
 // console.log( matchFinder(string1, string2));
+
+
+
+function sortMaker(arr) {
+   const [element1, element2] = arr;
+   if (typeof element1 !== "number" || typeof element2 !== "number") {
+      return "Please provide me  an array of numbers";
+   } else if (element1 < 0 || element2 < 0) {
+      return "Invalid Input";
+   } else if (element1 === element2) {
+      return "equal";
+   } else {
+      return [Math.max(element1, element2), Math.min(element1, element2)];
+   }
+
+}
+// const arr = [2,3] ;
+// const arr = [4,2] ;
+// const arr = [4,4] ;
+// const arr = [1,2] ;
+// const arr = [4,-2] ;
+// const arr = [0,1] ;
+
+
+// console.log(sortMaker(arr));
+
+function findAddress(obj) {
+   if (typeof obj !== "object") {
+      return "please provide me a valid object";
+   } else {
+      const street = obj.street || "__";
+      const house = obj.house || "__";
+      const society = obj.society || "__";
+      return street+ ","+house+ ","+society;
+   }
+}
+// const obj = {
+//    street: 10,
+//    house: "15A",
+//    society: "Earth Perfect"
+// }
+// console.log (findAddress(obj));
+
+function canPay(changeArray, totalDue) {
+if (changeArray.length ===0){
+   return false;
+}else {
+   let totalChangeAmount = 0 ;
+   for (let i = 0;i<changeArray.length; i++){
+      totalChangeAmount += changeArray[i];
+   }
+   return totalChangeAmount>=totalDue;
+}
+
+}
+const changeArray=[1,2,5];
+// const changeArray=[1,5,5];
+
+const totalDue =6;
+console.log(canPay(changeArray, totalDue));
