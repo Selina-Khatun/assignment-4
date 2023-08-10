2ff073a52eec857df2d946f91350667b2f588ff0
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2023-08-11 01:57:11 +0600

    batch 6 assignment -4

--- assignment.js
+++ assignment.js
@@ -7,9 +7,6 @@ function cubeNumber(number) {
       return result;
    }
 }
-// console.log(cubeNumber(p));
-// console.log(cubeNumber(3));
-
 
 function matchFinder(string1, string2) {
    if (typeof string1 !== "string" || typeof string2 !== "string") {
@@ -19,25 +16,7 @@ function matchFinder(string1, string2) {
    } else {
       return false;
    }
-
 }
-// const string1 = "JavaScript";
-// const string2 = "Code";
-
-// const string1 = "John Doe";
-// const string2 = "ohn";
-
-// const string1 = 444;
-// const string2 = 3333;
-// const string1 = "Peter Parker";
-// const string2 = "Pen";
-
-// const string1 = "Peter Parker";
-// const string2 = "pet";
-
-// console.log( matchFinder(string1, string2));
-
-
 
 function sortMaker(arr) {
    const [element1, element2] = arr;
@@ -50,17 +29,7 @@ function sortMaker(arr) {
    } else {
       return [Math.max(element1, element2), Math.min(element1, element2)];
    }
-
 }
-// const arr = [2,3] ;
-// const arr = [4,2] ;
-// const arr = [4,4] ;
-// const arr = [1,2] ;
-// const arr = [4,-2] ;
-// const arr = [0,1] ;
-
-
-// console.log(sortMaker(arr));
 
 function findAddress(obj) {
    if (typeof obj !== "object") {
@@ -69,30 +38,135 @@ function findAddress(obj) {
       const street = obj.street || "__";
       const house = obj.house || "__";
       const society = obj.society || "__";
-      return street+ ","+house+ ","+society;
+      return street + "," + house + "," + society;
    }
 }
-// const obj = {
-//    street: 10,
-//    house: "15A",
-//    society: "Earth Perfect"
-// }
-// console.log (findAddress(obj));
 
 function canPay(changeArray, totalDue) {
-if (changeArray.length ===0){
-   return false;
-}else {
-   let totalChangeAmount = 0 ;
-   for (let i = 0;i<changeArray.length; i++){
-      totalChangeAmount += changeArray[i];
+   if (changeArray.length === 0) {
+      return "Invalid Input";
+   } else {
+      let totalChangeAmount = 0;
+      for (let i = 0; i < changeArray.length; i++) {
+         totalChangeAmount += changeArray[i];
+      }
+      return totalChangeAmount >= totalDue;
    }
-   return totalChangeAmount>=totalDue;
-}
 
 }
-const changeArray=[1,2,5];
-// const changeArray=[1,5,5];
+// What will be the output of the following JavaScript code?
 
-const totalDue =6;
-console.log(canPay(changeArray, totalDue));
+function compare(a, b) {
+   if (a == b) {
+      return true;
+   } else {
+      return false;
+   }
+}
+// const result = compare(15, "15");
+// console.log(result);
+function compare(a, b) {
+   if (a.toString() === b) {
+      return true;
+   } else {
+      return false;
+   }
+}
+const result = compare(25, 25);
+//   console.log(result);
+// What will be the output of the following JavaScript code? (Hint: Implicit type conversion)
+
+// console.log("123" + 123);
+// What will be the output of the following JavaScript code?
+
+let a = "hi";
+let b = "there";
+// console.log(a+b);
+
+
+function degreesToRadians(degrees) {
+   return degrees * (Math.PI / 180);
+}
+
+// Example usage
+const degrees = 90;
+const radians = degreesToRadians(degrees);
+//  console.log(`${degrees} degrees is approximately ${radians} radians.`);
+
+
+
+/*************************************************************************************/
+// batch 6 restart assignment---4
+/***************************************************************************************/
+
+
+function radianToDegree(degrees) {
+
+   if (typeof degrees !== "number") {
+      return "please provide me a valid number";
+   } else {
+      const piValue = 3.1415265358;
+      return degrees * piValue / 180;
+   }
+}
+// console.log(radianToDegree("180"));
+
+
+function isJavaScriptFile(fileName) {
+   if (typeof fileName !== "string") {
+      return "please provide me a valid file name ";
+   } else {
+      return fileName.toLowerCase().endsWith(".js");
+   }
+
+
+}
+console.log(isJavaScriptFile(5));
+function oilPrice(diesel, petrol, octane) {
+
+   if (typeof diesel !== "number" || typeof petrol !== "number" || typeof octane !== "number") {
+      return "please provide me the number in all input arguments ";
+   } else if (diesel < 0 || petrol < 0 || octane < 0) {
+      return "please provide me positive number";
+   } else {
+      const dieselPrice = 114;
+      const petrolPrice = 130;
+      const octanePrice = 135;
+      const totalPrice = dieselPrice * diesel + petrolPrice * petrol + octanePrice * octane;
+      return totalPrice;
+   }
+
+}
+// console.log(oilPrice(1, 1, 1));
+
+function publicBusFare(people) {
+
+   if (typeof people !== "number" || people < 0) {
+      return "Please provide me a positive number";
+   } else {
+      const reservedBus = 50;
+      const microBus = 11;
+      const publicBusCost = 250;
+      const busRemaining = people % reservedBus;
+      const microBusRemaining = busRemaining % microBus;
+      const totalBusFare = publicBusCost * microBusRemaining;
+      return totalBusFare;
+
+   }
+}
+// console.log(publicBusFare(263));
+function isBestFriend(obj1, obj2) {
+   if (typeof obj1 !== "object" || typeof obj2 !== "object") {
+      return "please provide me the valid objects in both input arguments";
+   } else if (obj1.friend === obj2.name && obj2.friend === obj1.name) {
+      return true;
+   } else {
+      return false;
+   }
+}
+// const obj1={name:'Tom',friend:'rock'};
+// const obj2={name:'rock',friend:'Tom'};
+// const obj3={name:'Tim',friend:'rocky'};
+// const obj4={name:'Tom',friend:'rock'};
+// console.log(isBestFriend(obj1,obj2));
+// console.log(isBestFriend(obj3,obj4));
